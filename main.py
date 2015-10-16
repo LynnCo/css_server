75fef37d1d6655741ddbd881f1c9e0432827596e
Lynn Cyrin <firemagelynn@gmail.com>
2015-10-15 22:29:10 -0400

    more stuff

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # builtin
 import os
+from glob import glob
+
 # external
 import flask
 import watchdog
+import subprocess
 from dotenv import load_dotenv
 
 
@@ -15,31 +18,35 @@ output_dir = 'static/css/'
 
 
 def build_css():
-    import subprocess
+    for sass_path in glob(assets_dir+'*.*'):
+        filename = sass_path.split(assets_dir)[-1].split('.')[0]
+        css_path = output_dir + filename + '.css'
 
-    args = {
-        'source': '{}main.scss'.format(assets_dir),
-        'output': '{}main.css'.format(output_dir),
-    }
-    subprocess.call('''
-        sassc -m {source} {output} -s compressed
-        '''.format(**args),
-        shell=True,
-        preexec_fn=os.setsid,
-        stdout=subprocess.PIPE
-    )
-    print('Built {}main.css'.format(output_dir))
+        print('Building {}'.format(css_path))
+
+        args = {
+            'source': '{}'.format(sass_path),
+            'output': '{}'.format(css_path),
+        }
+        subprocess.call('''
+            sassc -m {source} {output} -s compressed
+            '''.format(**args),
+            shell=True,
+            preexec_fn=os.setsid,
+            stdout=subprocess.PIPE
+        )
+    print('Completed Build')
 
 def watch_css():
     # build css on changes
     from watchdog.events import FileSystemEventHandler
-    class If_scss_changes (FileSystemEventHandler):
+    class If_sass_changes (FileSystemEventHandler):
         def on_modified (self, event): build_css()
 
     # monitor for changes
     from watchdog.observers import Observer
     watch = Observer()
-    watch.schedule(If_scss_changes(), assets_dir)
+    watch.schedule(If_sass_changes(), assets_dir)
     watch.start()
     print('Watching {} for changes'.format(assets_dir))
 
